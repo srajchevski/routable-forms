bd13cc06159010d548c4ddd95ac2e605ddab6237
srajchevski <srajchevski@gmail.com>
2020-02-18 19:40:50 +0100

    styling :lipstick:

--- src/components/form/multiselect/Multiselect.tsx
+++ src/components/form/multiselect/Multiselect.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import { Multiselect as MultiselectInput } from 'react-widgets'
+import InputContainer from '../inputContainer/InputContainer'
 
 const Multiselect: React.FC = ({
   input,
   data,
   meta: { touched, error },
-  placeholder,
+  placeholder = '',
   valueField,
   textField,
 }: any) => (
-  <MultiselectInput
-    {...input}
-    containerClassName={`multiselect ${touched && error ? 'error' : ''}`}
-    placeholder={placeholder}
-    onBlur={() => input.onBlur()}
-    value={input.value || []}
-    data={data}
-    valueField={valueField}
-    textField={textField}
-  />
+  <InputContainer>
+    {placeholder && input.value && input.value && (
+      <label className="label-placeholder" style={{ marginBottom: 0 }}>
+        {placeholder}
+      </label>
+    )}
+    <MultiselectInput
+      {...input}
+      containerClassName={`multiselect ${touched && error ? 'error' : ''}`}
+      placeholder={placeholder}
+      onBlur={() => input.onBlur()}
+      value={input.value || []}
+      data={data}
+      valueField={valueField}
+      textField={textField}
+    />
+  </InputContainer>
 )
 
 export default Multiselect
